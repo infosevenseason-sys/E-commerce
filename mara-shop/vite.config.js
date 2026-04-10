acee561916f8700768042c1d80e71cf7fdda290d
infosevenseason-sys <info.sevenseason@gmail.com>
2026-04-10 20:57:35 +0530

    updated build with relative path

--- mara-shop/vite.config.js
+++ mara-shop/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Aa line Hostinger par 404 error sudharva mate che
+  base: './', 
 })
