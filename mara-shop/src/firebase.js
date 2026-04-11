7f206380d2639b53b39d6a55bea3b575b87374c5
infosevenseason-sys <info.sevenseason@gmail.com>
2026-04-11 13:33:15 +0530

    corrected appId and apiKey from console

--- mara-shop/src/firebase.js
+++ mara-shop/src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5TwHuLtmqaUZduOc4aSllMpvJsLiuIYA", 
+  apiKey: "AIzaSyD5TwHuLtmqaUZduOc4aSllMpvJsLiuIYA",
   authDomain: "e-commerce-c9973.firebaseapp.com",
   projectId: "e-commerce-c9973",
   storageBucket: "e-commerce-c9973.firebasestorage.app",
   messagingSenderId: "414543507361",
-  appId: "1:414543507361:web:b56cc13a97f80f86b7a5e0",
+  appId: "1:414543507361:web:b56cc13a97f80f66b7a5e0",
   measurementId: "G-7H1RL8F712"
 };
 
